811909a9871eab7aa380eab7fd068e3eb3a6035e
malak <malak-y@live.com>
2020-02-06 16:22:03 +0000

    Display a count of categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
     @product=Product.count() 
+    @category=Category.count()
   end
 end
