7a4295247f263f4f752cf02b2bf440a5eeb98b8b
malak <malak-y@live.com>
2020-02-06 16:16:53 +0000

    display the count of products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
  
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @product=Product.count() 
   end
 end
