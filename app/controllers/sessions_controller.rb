48c3b2abf1d35f3385377b3a404cd6fe7797251b
malak <malak-y@live.com>
2020-02-11 18:56:14 +0000

    Capybara and Poltergeist Setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    print User.authenticate_with_credentials(params[:email], params[:password])
       if user = User.authenticate_with_credentials(params[:email], params[:password])
         session[:user_id]=user.id
       redirect_to '/'
